611853f2491ecf849ee5078751dfcc18fb6aca86
Student <jsitbon@qcc.cuny.edu>
2019-07-11 17:55:10 -0400

    all

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+  constructor(props){
+    super(props)
+    this.state={
+      ticker: 0,
+      increase: true
+    }
+  }
+
+
+
+  setTicker =() => {
+    let newTicker = this.state.ticker;
+    return newTicker + 1;
+  }
+
+ 
+  
+  componentDidMount(){
+    console.log('[App.js] componenetDidMount')
+    let counter = this.state.ticker; 
+    (this.state.increase)? (setInterval(()=>(
+        this.setState({
+          ticker: this.state.ticker + 1
+        })
+      ), 1000)): console.log('this is another thing');
+    
+    console.log(counter) ; 
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    
+    if(nextState.ticker % 3 === 0) return true;
+    else return false;
+  }
+
+  tickReset(){
+
+    this.setState({
+      ticker: 0
+    })
+  
+  }
+
+  tickPause(){
+    
+   this.setState({
+     increase: !this.state.increase 
+   })
+
+  }
+
+
+  render(){
+      return <div className="App">
+        ticker: {this.state.ticker}
+        <br/>
+        <button onClick={()=>this.tickReset()}>Reset</button>
+        <button onClick={()=>this.tickPause()}>Pause</button>
+        {/* <button onClick>increase 3 times</button>  */}
+
+        </div>
+    
+  }
+  
 }
 
 export default App;
